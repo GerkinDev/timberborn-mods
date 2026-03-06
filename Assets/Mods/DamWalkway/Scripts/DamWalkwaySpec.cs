113e276ad9b2abff9a37caf837ecf571db51751d
Alexandre GERMAIN-RAFFIER <alexandre.j.p.germain@gmail.com>
2026-03-06 02:44:27 +0100

    feat: add basic spec

---
+++ Assets/Mods/DamWalkway/Scripts/DamWalkwaySpec.cs
@@ -0,0 +1,19 @@
+﻿using Timberborn.BlueprintSystem;
+using UnityEngine;
+
+namespace GerkinDev.DamWalkway.Assets.Mods.DamWalkway.Scripts
+{
+    /// <seealso href="https://github.com/mechanistry/timberborn-modding/wiki/Timberborn-architecture#specs"/>
+    internal record DamWalkwaySpec : ComponentSpec
+    {
+        [Serialize] public string Anchor { get; set; }
+        [Serialize] public GateTransformSpec OpenTransform { get; init; }
+        [Serialize] public GateTransformSpec CloseTransform { get; init; }
+    }
+
+    public record GateTransformSpec
+    {
+        [Serialize] public Vector3 Position { get; init; }
+        [Serialize] public Vector3 Rotation { get; init; }
+    }
+}
